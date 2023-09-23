ab29412aecd1ebfd3e2ee2aa78c0515a5ae6ac2d
Gabri0710 <racitigabriele@gmail.com>
2023-09-23 21:37:11 +0200

    added activityRecognition

---
+++ App/app/src/main/java/com/example/geo_fencing_basedemergencyadvertising/ActivityRecognitionService.java
@@ -0,0 +1,55 @@
+package com.example.geo_fencing_basedemergencyadvertising;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+public class ActivityRecognitionService extends IntentService {
+    public ActivityRecognitionService() {
+        super("ActivityRecognitionService");
+
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("Ciao", "ciao");
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+            int activityType = mostProbableActivity.getType();
+            int confidence = mostProbableActivity.getConfidence();
+
+            String activityName = getActivityName(activityType);
+
+            String message = "Attività rilevata: " + activityName + " (Confidenza: " + confidence + ")";
+            Log.d("ActivityRecognition", message);
+
+        }
+    }
+
+    private String getActivityName(int activityType) {
+        switch (activityType) {
+            case DetectedActivity.IN_VEHICLE:
+                return "In veicolo";
+            case DetectedActivity.ON_BICYCLE:
+                return "In bicicletta";
+            case DetectedActivity.ON_FOOT:
+                return "A piedi";
+            case DetectedActivity.RUNNING:
+                return "Correre";
+            case DetectedActivity.STILL:
+                return "Fermo";
+            case DetectedActivity.TILTING:
+                return "Inclinato";
+            case DetectedActivity.UNKNOWN:
+                return "Sconosciuto";
+            case DetectedActivity.WALKING:
+                return "Camminare";
+            default:
+                return "Sconosciuto";
+        }
+    }
+}
--- App/app/src/main/java/com/example/geo_fencing_basedemergencyadvertising/MainActivity.java
+++ App/app/src/main/java/com/example/geo_fencing_basedemergencyadvertising/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.geo_fencing_basedemergencyadvertising;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityRecognitionClient activityRecognitionClient;   //client riconoscimento attività
+    private PendingIntent pendingIntent;    //intent per ricevere i risultati del riconoscimento
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityRecognitionClient = ActivityRecognition.getClient(this);
+
+        Intent intent = new Intent(this, ActivityRecognitionService.class);
+        pendingIntent = PendingIntent.getService(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        // Richiedi le attività rilevate
+        activityRecognitionClient.requestActivityUpdates(5000, pendingIntent);
+        Log.d("Test", "Test");
     }
 }
