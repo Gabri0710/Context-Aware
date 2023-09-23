edf80a8700f16ad6868b8bf65aad188fff11960f
Gabri0710 <racitigabriele@gmail.com>
2023-09-23 18:47:56 +0200

    create project

---
+++ App/app/src/main/java/com/example/geo_fencing_basedemergencyadvertising/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.geo_fencing_basedemergencyadvertising;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
