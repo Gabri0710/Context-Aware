ec9ce70872fa55f5ee1f2e61ca7f818d330191ac
Gabri0710 <racitigabriele@gmail.com>
2023-09-26 15:28:29 +0200

    addded comunication from backend to database

--- Backend/app.py
+++ Backend/app.py
@@ -6,16 +6,17 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 # Configuro la connessione con il database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://password:password@localhost/geofance'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/geofance'
 db = SQLAlchemy(app)
 
 i = 0
 
 # Definisco il modello per la tabella degli utenti
 class UserCoordinate(db.Model):
-    __tablename__ = 'user_coordinates'
+    __table_args__ = {'schema': 'Coordinate'}  # Imposta lo schema qui
+    __tablename__ = 'geofance-coordinate'  # Il nome della tabella senza lo schema
     user_id = db.Column(db.Integer, primary_key=True)
-    coordinates = db.Column(Geometry(geometry_type='POINT', srid=4326))
+    posizione = db.Column(Geometry(geometry_type='POINT', srid=4326))
 
 
 @app.route('/')
@@ -28,21 +29,34 @@ def upload_location():
         # Ottiengo i dati di posizione dalla richiesta POST
         data = request.get_json()
 
-        print("TESTTTTTTTTTTTTTTTTTTT")
+        
 
         # Estrai le coordinate latitudine e longitudine dai dati
         latitude = data.get('latitude')
         longitude = data.get('longitude')
 
+        
+        
+
         #ottengo l'username dell'utente (DA CAMBIARE, MOMENTANEO PER TEST)
         user_id = i
-        i+=1
+
+        print(latitude)
+        print(longitude)
 
         # Creo un oggetto Point con le coordinate
         coordinates = f'POINT({longitude} {latitude})'
+        #coordinates = "POINT("
+        #coordinates+=str(longitude)
+        #coordinates+=str(latitude)
+        #coordinates+=")"
+
+        
 
         # Crea un nuovo record nella tabella
-        new_user_coordinate = UserCoordinate(user_id=user_id, coordinates=coordinates)
+        new_user_coordinate = UserCoordinate(user_id=user_id, posizione=coordinates)
+
+        print("TESTTTTTTTTTTTTTTTTTTT")
 
         try:
             # Codice per l'inserimento del record
@@ -57,7 +71,7 @@ def upload_location():
         print(f"Latitude: {latitude}, Longitude: {longitude}")
 
         # Puoi elaborare ulteriormente i dati o inviare una risposta al client Android se necessario
-
+        #i+=1
         return jsonify({"message": "Dati di posizione ricevuti correttamente."}), 200
 
     except Exception as e:
