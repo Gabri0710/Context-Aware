a7af11f84776ae05c4103d6634bac52186398348
Gabri0710 <racitigabriele@gmail.com>
2023-10-22 21:36:06 +0200

    refactor js code

--- Test-Docker-2PUNTIEXTRA/Frontend/script_add_server.js
+++ Test-Docker-2PUNTIEXTRA/Frontend/script_add_server.js
@@ -1,30 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //oggetto Feature dove memorizzo i punti scelti
+    //oggetto Feature dove memorizzo il punto scelto dove inserire il server
     var selectedPoints = new ol.Collection();
 
-    //oggetto Source, mi serve per prendere in input delle Feature e passarle a un Layer successivamente
+    //oggetto Source associato alle feature dei punti
     var vectorPoints = new ol.source.Vector({
         features: selectedPoints
     });
 
-    //Layer dei punti scelti
+    //Layer dei punti selezionati
     var pointsLayer = new ol.layer.Vector({
         source: vectorPoints,
         style: new ol.style.Style({
             image: new ol.style.Circle({
               radius: 6,
               fill: new ol.style.Fill({
-                color: 'red' // Colore di riempimento del cerchietto
+                color: 'red'
               }),
               stroke: new ol.style.Stroke({
-                color: 'black', // Colore del bordo del cerchietto
+                color: 'black',
                 width: 2
               })
             })
         })
     });
     
+    //layer dei server
+    var serverLayer;
     
     //Layer di OpenStreetMap
     var osmLayer=new ol.layer.Tile({
@@ -33,6 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     osmLayer.setVisible(true);
 
+    //inizializzo la mappa con i layer scelti
     var map = new ol.Map({
         target: 'map',
         layers: [osmLayer, pointsLayer], 
@@ -40,15 +43,15 @@ document.addEventListener('DOMContentLoaded', function() {
             center: ol.proj.fromLonLat([11.3394883, 44.4938134]),
             zoom: 13, 
         })
-        });
+    });
     
     
-
+    //al click
     map.on('click', function(event) {
-                if (event.originalEvent.ctrlKey) {
+                if (event.originalEvent.ctrlKey) {                                                      //se ho ctrl premuto, tolgo il punto
                     selectedPoints.pop()
                 }
-                else if (selectedPoints.getLength() === 0) {
+                else if (selectedPoints.getLength() === 0) {                                            //se non ho ancora scelto il punto, lo inserisco
                     var coordinate = event.coordinate;
                     var pointFeature = new ol.Feature(new ol.geom.Point(coordinate));
                     selectedPoints.push(pointFeature);
@@ -56,16 +59,20 @@ document.addEventListener('DOMContentLoaded', function() {
         
     });
 
-    var serverLayer;
+    
 
+    //funzione che carica i server già presenti
     function loadServer(){
             fetch("http://localhost:5001/get_server", {
             method: "GET"
             })
             .then(response => response.json())
             .then(data => {
-                //console.log(data)
+
+                //tolgo i layer presenti precedentemente
                 map.removeLayer(serverLayer);
+
+                //inizializzo le feature con il punto selezionato
                 var serverFeatures = data.map(item => {
                     const coordinates = item.geometry.coordinates;
                     const point = new ol.Feature({
@@ -74,39 +81,40 @@ document.addEventListener('DOMContentLoaded', function() {
                     return point;
                 });
             
-                // Creazione del livello vettoriale
+                // source associata al server
                 serverSource = new ol.source.Vector({
                     features: serverFeatures,
                 });
                 
-            
+                
+                //layer associato al server
                 serverLayer = new ol.layer.Vector({
                     source: serverSource,
                     style: new ol.style.Style({
                         image: new ol.style.Circle({
                             radius: 7,
                             fill: new ol.style.Fill({
-                                color: 'black', // Colore del punto
+                                color: 'black',
                             }),
                         }),
                     }),
                 });
             
-                // Aggiungi il livello vettoriale alla mappa
+                // Aggiungo il layer
                 map.addLayer(serverLayer);
             })
             .catch(error => {
-                // Gestisci gli errori
                 console.error("Errore:", error);
             });
         
 
     }
 
+    //aggiunta di un server al click del pulsante
     document.getElementById("addServerForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // impedisce alla pagina di ricaricarsi quando viene inviato il modulo
+        event.preventDefault();                                 // impedisce alla pagina di ricaricarsi quando viene inviato il modulo
 
-        var formData = new FormData(this); // Creo un oggetto FormData
+        var formData = new FormData(this);              
 
         //definisco le proiezioni. Sul web si lavora con EPSG:3857 ma devo convertirlo a EPSG:4326 per lavorare con le nostre coordinate note
         var projection3857 = ol.proj.get('EPSG:3857');
@@ -114,10 +122,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         selectedPoints.forEach(function(feature) {
             // Ottengo le coordinate della feature
-            
             var coordinates = ol.proj.transform(feature.getGeometry().getCoordinates(), projection3857, projection4326);
             
-            // Aggiungo le coordinate alla FormData con chiave coordinates
+            // Aggiungo le coordinate alla FormData
             formData.append('coordinates', coordinates);
             
         });
@@ -129,11 +136,10 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(response => response.json())
         .then(data => {
-            selectedPoints.clear();
-            loadServer();
+            selectedPoints.clear();                                     //rimuovo il punto dopo averlo mandato
+            loadServer();                                               //ricarico i server
         })
         .catch(error => {
-            // Gestisco gli errori
             console.error("Errore:", error);
         });
 
